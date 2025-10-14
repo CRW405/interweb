200e25765340a47d4a51f0803d7a3e24fdf438b5
CRW405 <134819932+CRW405@users.noreply.github.com>
2025-10-13 21:25:25 -0500

    add window titles to each window and update styles for window list
    TODO: stye window list buttons

--- scripts/addWindowTitles.js
+++ scripts/addWindowTitles.js
@@ -1,18 +1,19 @@
-const windows = document.querySelectorAll(".window")
-const names = Array.from(windows).map(element => element.id ? element.id : element.tagName.toLowerCase());
+(function () {
+    const windows = document.querySelectorAll(".window");
+    const names = Array.from(windows).map(element => element.id ? element.id : element.tagName.toLowerCase());
 
-const capitalize = string => string ? string.charAt(0).toUpperCase() + string.slice(1) : string;
-const capitalizedNames = names.map(capitalize);
+    const capitalize = string => string ? string.charAt(0).toUpperCase() + string.slice(1) : string;
+    const capitalizedNames = names.map(capitalize);
 
-const windowNamesString = capitalizedNames.join(", ");
-window.windowNamesString = windowNamesString;
-console.log(windowNamesString);
+    const windowNamesString = capitalizedNames.join(", ");
+    window.windowNamesString = windowNamesString;
+    // console.log(windowNamesString);
 
-window.forEach((element, i) => {
-    let title = capitalizedNames[i]
-
-    let titleContainer = document.createElement("div")
-    titleContainer.className = "window-title"
-    element.prepend(titleContainer)
-    titleContainer.textContent=title
-});
+    windows.forEach((element, i) => {
+        const title = capitalizedNames[i];
+        const titleContainer = document.createElement("div");
+        titleContainer.className = "window-title";
+        titleContainer.textContent = title;
+        element.prepend(titleContainer);
+    });
+})();
--- scripts/populateWindowList.js
+++ scripts/populateWindowList.js
@@ -1,9 +1,23 @@
 const windowList = document.getElementById("window-list")
 const ul = windowList.firstElementChild
 
-const windows = document.querySelectorAll(".windows")
+const windows = document.querySelectorAll(".window")
 
 for (const window of windows) {
     let li = document.createElement("li")
     let button = document.createElement("button")
+
+    let titleContainer = window.querySelector(".window-title")
+    let title = titleContainer ? titleContainer.textContent.trim() : (window.id || window.tagName.toLowerCase())
+    // console.log(title);
+
+    button.textContent = title;
+
+    button.addEventListener("click", () => {
+        window.style.display = "block"
+    })
+
+    li.appendChild(button)
+    ul.appendChild(li)
+    
 }
