6a63ae013c69cba27083d8036dd9701d9ca52b62
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-01-08 14:14:25 -0600

    random github repos save the day once again

--- scripts/visitCounter.js
+++
@@ -1,7 +0,0 @@
-const url = "https://api.counterapi.dev/v1/crw405s-team-2424/katzmo/hit";
-
-fetch(input)
-  .then((res) => res.json())
-  .then((data) => {
-    document.getElementById("visit-count").innerText = data.value;
-  });
